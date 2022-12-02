62f8f34881cbd5c35415f3d65d966c2c55115efc
Eisim <koctik.w@mail.ru>
2022-12-02 23:58:17 +0300

    .

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,11 +1,7 @@
 #include <gtest.h>
-#include<iostream>
 
 int main(int argc, char **argv) {
-    char tmp;
     ::testing::InitGoogleTest(&argc, argv);
-    RUN_ALL_TESTS();
-    std::cin >> tmp;
-    return 1;
+    return RUN_ALL_TESTS();;
     
 }
